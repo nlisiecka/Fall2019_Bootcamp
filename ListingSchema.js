c6f9cd3576e9177416305a5a83fa5bdaed9942d7
Natalia Lisiecka <nlisiecka@ufl.edu>
2019-09-15 16:15:03 -0400

    changes added

--- ListingSchema.js
+++ ListingSchema.js
@@ -9,13 +9,6 @@ var mongoose = require('mongoose'),
 var listingSchema = new Schema({
   /* Your code for a schema here */ 
   //Check out - https://mongoosejs.com/docs/guide.html
-
-});
-
-/* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
-   See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
-*/
-listingSchema.pre('save', function(next) {
   code : String;
   name : String;
   coordinates: {
@@ -23,6 +16,14 @@ listingSchema.pre('save', function(next) {
     longitude : Number;
   }
   address : String;
+
+});
+
+/* Create a 'pre' function that adds the updated_at (and created_at if not already there) property 
+   See https://scotch.io/tutorials/using-mongoosejs-in-node-js-and-mongodb-applications
+*/
+listingSchema.pre('save', function(next) {
+
 
 });
 
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: {
+    uri: 'mongodb+srv://guest:<password>@cluster0-xucs2.mongodb.net/test?retryWrites=true&w=majority', //place the URI of your mongo database here.
+  }
+};
